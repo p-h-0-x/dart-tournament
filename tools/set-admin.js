5c509646d7b075d74d889fbe97890afe84bc1115
Ph0xel <merieaub@gmail.com>
2026-03-12 14:34:47 +0100

    feat: add tools to add admin

---
+++ tools/set-admin.js
@@ -0,0 +1,12 @@
+// set-admin.js
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+admin.auth().getUserByEmail('merieaub@gmail.com')
+  .then(user => admin.auth().setCustomUserClaims(user.uid, { admin: true }))
+  .then(() => console.log('Done! User must sign out and back in.'))
+  .catch(console.error);
